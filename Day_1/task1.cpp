9085968f2bd133d34592fee59a8d382ca49c3129
RadadiyaAbhay <abhay592004@gmail.com>
2023-05-04 09:06:53 +0530

    cpp first day learning

---
+++ Day_1/task1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+main(){
+	char a[1000],c[20],d[1000],e[1000];
+	int b;
+	cout<<"Enter Your Surname = ";
+	cin>>a;
+	cout<<"Enter Your Name = ";
+	cin>>d;
+	cout<<"Enter Your Father Name = ";
+	cin>>e;
+	cout<<"Enter Your Age = ";
+	cin>>b;
+	cout<<"Enter Your Contact No. = ";
+	cin>>c;
+	
+	cout<<"Your Full Name = "<<a<<" "<<d<<" " <<e<<endl<<"Your Age = "<<b<<endl<<"Your Contact No. = "<<c;
+}
